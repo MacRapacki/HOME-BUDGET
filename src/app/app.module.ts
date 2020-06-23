97eb49f2be9aa005d71eb060ea815eafe4d128e9
Maciej Rapacki <mac.rapacki@gmail.com>
2020-06-23 21:13:41 +0100

    initial commit create and style navigation and status bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { StatusBarComponent } from './status-bar/status-bar.component';
+import { FoodComponent } from './pages/food/food.component';
+import { CarComponent } from './pages/car/car.component';
+import { PersonalComponent } from './pages/personal/personal.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PhoneComponent } from './pages/phone/phone.component';
+import { BankComponent } from './pages/bank/bank.component';
+import { HealthComponent } from './pages/health/health.component';
+import { SportComponent } from './pages/sport/sport.component';
+import { IncomeComponent } from './pages/income/income.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    StatusBarComponent,
+    FoodComponent,
+    CarComponent,
+    PersonalComponent,
+    HomeComponent,
+    PhoneComponent,
+    BankComponent,
+    HealthComponent,
+    SportComponent,
+    IncomeComponent
   ],
   imports: [
     BrowserModule,
