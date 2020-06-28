0b8e5a50840baca538444f299d28dc3b320caa4d
Maciej Rapacki <mac.rapacki@gmail.com>
2020-06-28 11:05:37 +0100

    create income page

--- src/app/pages/income/income.component.ts
+++ src/app/pages/income/income.component.ts
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncomeComponent implements OnInit {
 
+  arr = [
+    {
+      listNumber: "#",
+      date: "Date",
+      source: "Source",
+      amount: "Amount",
+
+    },
+  ];
+
+  date = new Date().toLocaleDateString('en-CA');
+  sourceName: string = "";
+  incomeAmount: any = 0
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /// show form on click
+  showForm() {
+    document.querySelector(".form").classList.toggle("active")
+  }
+
+  ///add incom info to list and update array
+  addItem(e) {
+    e.preventDefault();
+
+    if (this.sourceName === "" || this.incomeAmount === 0) { return alert('Fill the form.') };
+
+    const listItem = {
+      listNumber: "#",
+      date: this.date,
+      source: this.sourceName,
+      amount: this.incomeAmount,
+    };
+
+    this.arr.push(listItem);
+
+
+
+
+    this.sourceName = "";
+    this.incomeAmount = 0
+
   }
 
 }
